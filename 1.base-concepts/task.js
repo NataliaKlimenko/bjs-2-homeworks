54bda80a01890902c9b2381c0924f614b9393a29
Natalia Klimenko <Arhipnat84@gmail.com>
2026-04-11 18:12:51 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,40 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a) , (-b - Math.sqrt(d)) / (2 * a)];
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = parseFloat(percent);
+  contribution = parseFloat(contribution)
+  amount = parseFloat(amount);
+  countMonths = parseFloat(countMonths);
+
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+
+  let creditBody  = amount - contribution;
+  if (creditBody <= 0)
+    return 0;
+
+  let monthRate = (percent/ 100) / 12;
+  let coeffic = (1 + monthRate) ** countMonths;
+  let monthPayment = creditBody * (monthRate + (monthRate / (coeffic - 1)));
+  let total = monthPayment * countMonths;
+
+  total = Math.round(total * 100) / 100;
+
+  return total;
+
 }
